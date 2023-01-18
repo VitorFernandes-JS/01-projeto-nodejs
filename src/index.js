45de1b62359869e68a75b9a8eb4b18ad71f9cf29
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2023-01-17 22:30:57 -0300

    fix: add middleware in "/statement"

--- src/index.js
+++ src/index.js
@@ -1,23 +1,50 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
-
 const app = express();
-app.use(express.json());
-
 const customers = []; // Array para guardar os usuários
+
+// Middleware, basicamente é algo que fica no meio, entre a requisição e a resposta, e ele geralmente é usado para validar um token e etc, é basicamente uma função que valida a requisição antes de retornar a resposta
+// Esse parametro "next", define se a função vai pra frente ou não
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Não há extrato!" });
+  }
+
+  return next();
+}
+
+app.use(express.json());
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some(
+    // some retorna true ou false, ou seja aqui queremos saber se tem um cpf no array igual ao que estamos tentando colocar dentro dele
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "CPF já cadastrado!" });
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   });
 
-  return console.log(customers), response.status(201).send();
+  return (
+    console.log(customers),
+    response.status(201).json({ error: "CPF cadastrado com sucesso!" }).send()
+  );
+});
+
+app.get("/statement", (request, response) => {
+  verifyIfExistsAccountCPF()
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
